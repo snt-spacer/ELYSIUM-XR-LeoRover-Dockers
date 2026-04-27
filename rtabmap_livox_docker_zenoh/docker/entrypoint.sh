c0484191ad43701e51cc4c2dfb5b644b47b496a5
XR Development <xr-dev@ll202.localdomain>
2026-04-27 15:18:04 +0200

    update entrypoint to launch rtabmap with MID 360

--- rtabmap_livox_docker_zenoh/docker/entrypoint.sh
+++ rtabmap_livox_docker_zenoh/docker/entrypoint.sh
@@ -19,21 +19,7 @@ colcon build
 source "/home/leo/ros2_ws/install/setup.bash"
 
 # Start RTABMAP
-# (
-# ros2 launch rtabmap_livox rtabmap_livox_projection.launch.py
-# ) & RTB_PID=$!
-
-# Start Nav2
-#(
-#ros2 launch rtabmap_livox custom_nav.launch.py params_file:=/home/leo/nav2_params_repo.yaml # Original
-
-#ros2 launch rtabmap_livox custom_nav.launch.py params_file:=/home/leo/nav2_params_repo_testing.yaml
-#) & NAV_PID=$!
-
-
-# Bridge DDS topics (testing)
-# export ZENOH_CONFIG_OVERRIDE='mode="client";connect/endpoints=["192.168.88.102"];plugins/ros2dds/__required__=true;plugins/ros2dds/domain=4;plugins/ros2dds/namespace="/leo04"'
-# ros2 run rmw_zenoh_cpp rmw_zenohd
-# zenoh-bridge-ros2dds
+ros2 launch rtabmap_livox rtabmap_livox.launch.py
 
 exec "$@"
+          
